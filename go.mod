24c9926d0cafd74e9607e32c600ea71d219445d3
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-18 17:41:52 +0700

    Menambah Dependency

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/aziemp66/App-Say-Hello
 
 go 1.19
+
+require github.com/aziemp66/Go-Say-Hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/aziemp66/Go-Say-Hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
