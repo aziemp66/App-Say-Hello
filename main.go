76d4aa2f189e5cfb7e971d1d363e693948dba2ab
Azie Melza Pratama <aziemp66@gmail.com>
2022-10-18 18:15:57 +0700

    Major Upgrade Dependency

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	go_say_hello "github.com/aziemp66/Go-Say-Hello"
+	go_say_hello "github.com/aziemp66/Go-Say-Hello/v2"
 )
 
 func main() {
-	fmt.Println(go_say_hello.SayHello())
+	fmt.Println(go_say_hello.SayHello("Azie"))
 }
